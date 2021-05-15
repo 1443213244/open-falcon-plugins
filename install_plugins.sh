43beef65ced75845d61c94726dd1e37a6a7fb012
1443213244 <1443213244@qq.com>
2021-05-15 17:46:21 +0800

    Update install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -2,5 +2,5 @@
 pid=`ps -ef|grep server.py|grep python|awk '{print $2}'`
 port=`ss -lnp |grep pid=$pid|grep tcp|sort -g|awk -F" " '{ print $5}'|awk -F":" '{ print $2}'|sort -g|head -1`
 let end=port+252
-sed -i 's/12269/'$port'/g'  /home/work/open-falcon/open-falcon-plugins/liuliang/60_liuliang.py
-sed -i 's/12522/'$end'/g' /home/work/open-falcon/open-falcon-plugins/liuliang/60_liuliang.py
+sed -i 's/12269/'$port'/g'  /home/work/open-falcon/plugin/liuliang/60_liuliang.py
+sed -i 's/12522/'$end'/g' /home/work/open-falcon/plugin/liuliang/60_liuliang.py
